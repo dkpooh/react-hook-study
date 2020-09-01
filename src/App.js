af1594320bccc973f221ba10da86215b37b3ef5f
LimDoKyoung <limdk@LimDoKyoungui-MacBookPro.local>
2020-09-02 00:42:28 +0900

    useState study

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [ items, setItems ] = useState([
+    {
+      name: 'apple',
+      isActive: false,
+    },
+    {
+      name: 'banna',
+      isActive: false,
+    },
+    {
+      name: 'pineapple',
+      isActive: false,
+    },
+  ])
+  const [ value, setValue ] = useState('')
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    console.log(value)
+  }
+
+  const handleClick = (idx) => {
+    setItems(
+      items.map((ele, key) => (
+        {
+          ...ele,
+          isActive: idx === key ? !ele.isActive : ele.isActive
+        }
+    )))
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <input type="text" onChange={handleChange} />
+        <button onClick={handleSubmit}>submit</button>
+      </div>
+      <div>
+        <table>
+          <tbody>
+            {items.map((ele, idx) => (
+              <tr key={idx}>
+                <td>{ele.name}</td>
+                <td><button onClick={() => handleClick(idx)}>{ele.isActive ? '빼기' : '담기'}</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+export default App;
